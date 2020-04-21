8c97dfadd60b0e9afde3bec89421b8eb617e4107
goncy <gonzalo.pozzo4@gmail.com>
2020-04-21 18:54:45 -0300

    Restore .env

--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = {}
+require("dotenv").config({ path: '.env' });
+
+module.exports = {
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    CLOUDINARY_CLOUD: process.env.CLOUDINARY_CLOUD,
+    CLOUDINARY_PRESET: process.env.CLOUDINARY_PRESET,
+  }
+}
