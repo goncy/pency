83c25a0ee88f33152e8071d2b4ef74478692e337
goncy <gonzalo.pozzo4@gmail.com>
2020-04-24 16:19:19 -0300

    Remove .env

--- storage/api.ts
+++ storage/api.ts
@@ -3,7 +3,7 @@ import {CloudinaryResponse} from "./types";
 export default {
   upload: (file: File) => {
     return fetch(
-      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD}/image/upload?upload_preset=${process.env.REACT_APP_CLOUDINARY_PRESET}`,
+      `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD}/image/upload?upload_preset=${process.env.CLOUDINARY_PRESET}`,
       {
         method: "PUT",
         body: file,
