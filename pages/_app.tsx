9c70434335d037bfd44dc52c268e2f4b8aae7d3d
goncy <gonzalo.pozzo4@gmail.com>
2020-04-30 08:27:32 -0300

    Add new meta content type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -61,7 +61,7 @@ function App({Component, pageProps}) {
           <Head>
             <link href="/favicon.ico" rel="icon" />
             <meta
-              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, target-densityDpi=device-dpi"
               name="viewport"
             />
             <meta content={tenant.color || "#00B5D8"} name="theme-color" />
