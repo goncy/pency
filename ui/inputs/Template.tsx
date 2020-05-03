800a2cd062bb8050efef8b430dec0e7df29c8d0c
goncy <gonzalo.pozzo4@gmail.com>
2020-05-03 09:30:24 -0300

    Add message disclaimer

--- ui/inputs/Template.tsx
+++ ui/inputs/Template.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Textarea, Stack, Box} from "@chakra-ui/core";
+import {Textarea, Stack, Box, Text, Button, Flex} from "@chakra-ui/core";
 import template from "lodash.template";
 
 interface Props {
@@ -9,6 +9,7 @@ interface Props {
 }
 
 const TemplateInput: React.FC<Props> = ({value, onChange, data}) => {
+  const [isEnabled, setEnabled] = React.useState(false);
   const preview = React.useMemo(() => {
     try {
       const compile = template(value);
@@ -24,18 +25,50 @@ const TemplateInput: React.FC<Props> = ({value, onChange, data}) => {
   }
 
   return (
-    <Stack spacing={4}>
-      <Textarea minHeight={48} value={value} onChange={handleChange} />
-      <Box
-        as="pre"
-        backgroundColor="primary.100"
-        fontFamily="sans-serif"
-        fontSize="14px"
-        padding={3}
-        whiteSpace="pre-wrap"
-      >
-        {preview}
-      </Box>
+    <Stack position="relative" spacing={4}>
+      {!isEnabled && (
+        <Flex
+          alignItems="center"
+          flexDirection="column"
+          height="100%"
+          justifyContent="center"
+          position="absolute"
+          width="100%"
+        >
+          <Box
+            background="black"
+            height="100%"
+            opacity={0.75}
+            position="absolute"
+            rounded="sm"
+            width="100%"
+          />
+          <Stack
+            backgroundColor="primary.50"
+            margin={4}
+            padding={4}
+            rounded="sm"
+            spacing={4}
+            zIndex={1}
+          >
+            <Text>Este es un campo avanzado, solo cambialo si sabés como hacerlo.</Text>
+            <Button onClick={() => setEnabled(true)}>Habilitar</Button>
+          </Stack>
+        </Flex>
+      )}
+      <Textarea isDisabled={!isEnabled} minHeight={48} value={value} onChange={handleChange} />
+      {isEnabled && (
+        <Box
+          as="pre"
+          backgroundColor="primary.50"
+          fontFamily="sans-serif"
+          fontSize="14px"
+          padding={3}
+          whiteSpace="pre-wrap"
+        >
+          {preview}
+        </Box>
+      )}
     </Stack>
   );
 };
