45ecedfcb8539c4b49107bc70e0c574e6dffc1bb
goncy <gonzalo.pozzo4@gmail.com>
2020-05-03 15:55:59 -0300

    Working version

--- cart/selectors.ts
+++ cart/selectors.ts
@@ -1,20 +1,47 @@
 import {Cart, CartItem} from "./types";
 
 import {getOptionsString, getPrice} from "~/product/selectors";
+import {groupBy} from "~/selectors/group";
 
 export function getSimplifiedCart(cart: Cart) {
-  return cart.map(({product}) => {
-    const {category, subcategory, title, description, options} = product;
+  const {options = [], ...rest} = groupBy(cart, (item) =>
+    item.product.options?.length ? "options" : item.product.id,
+  );
 
-    return {
-      category,
-      subcategory,
-      price: getPrice(product),
-      title,
-      description,
-      options: getOptionsString(options),
-    };
-  });
+  return Object.values(rest)
+    .reduce<any>((acc, items) => {
+      const {
+        id,
+        product: {price, category, subcategory, title, description},
+      } = items[0];
+
+      return acc.concat({
+        id,
+        category,
+        subcategory,
+        description,
+        count: items.length,
+        title: title,
+        price: Number(price) * items.length,
+      });
+    }, [])
+    .concat(
+      options.map(({id, product}) => {
+        const {category, subcategory, title, description, options} = product;
+
+        return {
+          id,
+          count: 1,
+          category,
+          subcategory,
+          price: getPrice(product),
+          title,
+          description,
+          options: getOptionsString(options),
+        };
+      }),
+    )
+    .filter(Boolean);
 }
 
 export function getTotal(cart: Cart): number {
