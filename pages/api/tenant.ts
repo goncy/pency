578f59d06142542bda6620249c99710605986cae
OscarVelandia <odvs12@gmail.com>
2020-05-09 00:57:57 -0300

    Manejo de error Firebasea
    Agrego manejo de error en el POST request para crear la tienda

--- pages/api/tenant.ts
+++ pages/api/tenant.ts
@@ -114,7 +114,15 @@ export default async (req, res) => {
     return api
       .create(email, password, slug)
       .then(() => res.status(200).json({success: true}))
-      .catch(({status, statusText}) => res.status(status).end(statusText));
+      .catch(({status, statusText, errorInfo: firebaseMessage}) => {
+        if (status) {
+          res.status(status).end(statusText);
+        } else {
+          const {message, code} = firebaseMessage;
+
+          res.status(400).json({code, message});
+        }
+      });
   }
 
   if (req.method === "PATCH") {
