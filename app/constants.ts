ab177aa615e5e81da498d86386b1aced50793645
goncy <gonzalo.pozzo4@gmail.com>
2020-05-15 10:07:45 -0300

    Improve meta tags

---
+++ app/constants.ts
@@ -0,0 +1,17 @@
+export const META = {
+  favicon: "/favicon.ico",
+  theme: "#FFFFFF",
+  title: "Pency - Tu tienda online",
+  description: "Ahorrá tiempo y dinero aceptando pedidos online con Pency",
+  appleicon: "/logo192.jpg",
+  keywords: "tienda, online, fácil, delivery, compra, venta",
+  author: "Pency",
+  twitter: "pencyapp",
+  url: "https://pency.app",
+  banner: {
+    url: "/og-image.jpg",
+    format: "image/jpeg",
+    width: "1200",
+    height: "630",
+  },
+};
---
+++ app/layouts/LandingLayout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Head from "next/head";
+
+import {META} from "../constants";
+
+const LandingLayout: React.FC = ({children}) => (
+  <>
+    <Head>
+      <link href={META.favicon} rel="icon" />
+      <link href={META.appleicon} rel="apple-touch-icon" />
+      <title>{META.title}</title>
+      <meta content={META.theme} name="theme-color" />
+      <meta content={META.description} name="description" />
+      <meta content={META.keywords} name="keywords" />
+      <meta content={META.author} name="author" />
+      <meta content="summary_large_image" name="twitter:card" />
+      <meta content={META.twitter} name="twitter:site" />
+      <meta content={META.twitter} name="twitter:creator" />
+      <meta content={META.url} property="og:url" />
+      <meta content="website" property="og:type" />
+      <meta content={META.title} property="og:title" />
+      <meta content={META.description} property="og:description" />
+      <meta content={META.banner?.url} property="og:image" />
+      <meta content={META.banner?.url} property="og:image:url" />
+      <meta content={META.banner?.format} property="og:image:type" />
+      <meta content={META.banner?.width} property="og:image:width" />
+      <meta content={META.banner?.height} property="og:image:height" />
+      <meta content={META.title} property="og:image:alt" />
+    </Head>
+    {children}
+  </>
+);
+
+export default LandingLayout;
