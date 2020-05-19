0f1bcc651222d22fb685ea139ce1a2573f93dbfe
Diego Duro <diegoduro@Diegos-MacBook-Air.local>
2020-05-18 23:12:18 -0300

    #65-Add slug and filename to Cloudinary image upload

--- storage/api.ts
+++ storage/api.ts
@@ -1,11 +1,13 @@
 import {CloudinaryResponse, Quality} from "./types";
 
 export default {
-  upload: (file: File, quality: Quality) => {
+  upload: (tenantSlug: String, file: File, quality: Quality) => {
+    const fileName = file.name
+    
     return fetch(
       `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD}/image/upload?upload_preset=${
         quality === "high" ? process.env.CLOUDINARY_PRESET_HIGH : process.env.CLOUDINARY_PRESET_LOW
-      }`,
+      }&public_id=${tenantSlug}-${fileName}`,
       {
         method: "PUT",
         body: file,
