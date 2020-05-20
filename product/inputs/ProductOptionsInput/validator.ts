7cd521bfcee8f833f1396b2780d345d39dcb2880
goncy <gonzalo.pozzo4@gmail.com>
2020-05-20 11:19:05 -0300

    Refactor options

--- product/inputs/ProductOptionsInput/validator.ts
+++ product/inputs/ProductOptionsInput/validator.ts
@@ -2,8 +2,6 @@ import {Option} from "../../types/options";
 
 export default {
   title: (options: Option[]) => !options?.some((option) => !option.title),
-  count: (options: Option[]) =>
-    !options?.some((option) => option.type === "multiple" && !option.count),
   optionsCount: (options: Option[]) =>
     !options?.some((option) => option.type === "multiple" && option.count > option.options.length),
   optionsPrice: (options: Option[]) =>
