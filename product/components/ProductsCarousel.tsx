916a9f6c1b5bd8cd0df34fd1b75d8fcfdf0878de
goncy <gonzalo.pozzo4@gmail.com>
2020-05-23 18:19:11 -0300

    Fix featured

--- product/components/ProductsCarousel.tsx
+++ product/components/ProductsCarousel.tsx
@@ -6,6 +6,7 @@ const ProductsCarousel: React.FC<GridProps> = ({children, ...props}) => (
     gridAutoFlow="column"
     gridGap={{base: 4, sm: 8}}
     overflowX="auto"
+    overflowY="hidden"
     paddingBottom={4}
     templateColumns="repeat(auto-fill, minmax(280px,1fr))"
     {...props}
