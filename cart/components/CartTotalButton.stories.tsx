90a3673c4bea3c5ff6558ec2d6fd7d46ef00c8a0
goncy <gonzalo.pozzo4@gmail.com>
2020-05-25 16:00:59 -0300

    Fix story

--- cart/components/CartTotalButton.stories.tsx
+++ cart/components/CartTotalButton.stories.tsx
@@ -4,13 +4,21 @@ import {Box} from "@chakra-ui/core";
 
 import CartTotalButton from "./CartTotalButton";
 
+const item = () => ({
+  id: faker.random.uuid(),
+  product: faker.random.uuid(),
+  count: faker.random.number(20),
+  category: faker.commerce.department(),
+  subcategory: faker.commerce.department(),
+  price: Number(faker.commerce.price()),
+  title: faker.commerce.productName(),
+  description: faker.lorem.paragraph(),
+  options: faker.commerce.product(),
+});
+
 export const full = () => (
   <Box maxWidth="340px">
-    <CartTotalButton
-      count={faker.random.number(20)}
-      total={faker.random.number(10000)}
-      onClick={() => {}}
-    />
+    <CartTotalButton items={[item(), item(), item()]} onClick={() => {}} />
   </Box>
 );
 
