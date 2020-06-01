06047728068e9d68ac28fc6374b5aab16755492d
goncy <gonzalo.pozzo4@gmail.com>
2020-06-01 11:07:28 -0300

    Fix select

--- ui/inputs/Select.tsx
+++ ui/inputs/Select.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import {Select as ChakraSelect, SelectProps} from "@chakra-ui/core";
 
 const Select: React.FC<SelectProps> = React.forwardRef((props, ref) => (
-  <ChakraSelect ref={ref} focusBorderColor="primary.300" paddingX={2} variant="filled" {...props} />
+  <ChakraSelect
+    ref={ref}
+    focusBorderColor="primary.300"
+    paddingLeft={2}
+    paddingRight={8}
+    variant="filled"
+    {...props}
+  />
 ));
 
 export default Select;
