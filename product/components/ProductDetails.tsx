604dc3315032e072979db11e301fa293be3048e1
Ariel Gerstein <ariel.ger@hotmail.com>
2020-06-07 00:02:11 -0300

    Move from Modal to Drawer for product details

--- product/components/ProductDetails.tsx
+++ product/components/ProductDetails.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import {
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  // DrawerCloseButton,
   Stack,
   Flex,
   Box,
-  Modal,
-  ModalOverlay,
-  ModalCloseButton,
-  ModalContent,
   Text,
   Button,
-  FormControl,
-  FormLabel,
   IconButton,
 } from "@chakra-ui/core";
 import styled from "@emotion/styled";
@@ -20,11 +18,6 @@ import {Product} from "../types";
 
 const GoBackButton = styled(IconButton)`
   transform: rotate(180deg);
-`;
-
-// Fix margin prop getting overwrited when using chakra style props
-const StyledModalContent = styled(ModalContent)`
-  margin: 0;
 `;
 
 interface Props {
@@ -41,163 +34,135 @@ export default function ProductDetails({product, add, onClose}: Props) {
   // add({...product, options})
 
   return (
-    <Modal isCentered isOpen={true} onClose={onClose}>
-      <ModalOverlay />
-      <StyledModalContent
-        display="flex"
-        flexDirection={{
-          base: "column",
-          md: "row",
-        }}
-        height={{
-          base: "100vh",
-          md: "26rem",
-        }}
-        maxWidth={{
-          md: "50rem",
-        }}
-        overflowY={{
-          base: "scroll",
-          md: "visible",
-        }}
-        position="relative"
-      >
-        {image && (
-          <Box
-            borderBottomWidth={{
-              base: 1,
-              md: 0,
-            }}
-            borderRightWidth={{
-              md: 1,
-            }}
-            position="relative"
-          >
-            <GoBackButton
-              aria-label="Close product details"
-              icon="arrow-forward"
-              ml={2}
-              mt={2}
-              position="absolute"
-              variant="ghost"
-              onClick={onClose}
-            />
-            <Box
-              backgroundImage={`url(${image})`}
-              backgroundPosition="center"
-              backgroundSize="cover"
-              height={{
-                base: 48,
-                md: "26rem",
-              }}
+    <ProductOptionsForm
+      options={options}
+      onSubmit={(...props) => {
+        console.log("on submit result", props);
+      }}
+    >
+      {({form, submit, isLoading, watch}) => {
+        const formValues = watch();
+
+        const optionsPrice = Object.keys(formValues)
+          .filter((key) => key.startsWith("options"))
+          .reduce((optionsTotal, optionKey) => {
+            const optionsTypeTotal = formValues[optionKey]
+              .map((option) => option.price || 0)
+              .reduce((a, b) => a + b, 0);
+
+            return optionsTotal + optionsTypeTotal;
+          }, 0);
+
+        const totalPrice = price * formValues.quantity + optionsPrice;
+
+        return (
+          <Drawer isOpen={true} placement="right" onClose={onClose}>
+            <DrawerOverlay />
+            <DrawerContent
+              maxWidth="auto"
               width={{
                 base: "100%",
-                md: "26rem",
+                md: "lg",
               }}
-            />
-          </Box>
-        )}
-        <ProductOptionsForm
-          options={options}
-          onSubmit={(...props) => {
-            console.log("on submit result", props);
-          }}
-        >
-          {({form, submit, isLoading, watch}) => {
-            const formValues = watch();
-
-            const optionsPrice = Object.keys(formValues)
-              .filter((key) => key.startsWith("options"))
-              .reduce((optionsTotal, optionKey) => {
-                const optionsTypeTotal = formValues[optionKey]
-                  .map((option) => option.price || 0)
-                  .reduce((a, b) => a + b, 0);
-
-                return optionsTotal + optionsTypeTotal;
-              }, 0);
-
-            const totalPrice = price * formValues.quantity + optionsPrice;
-
-            return (
-              <Box position="relative">
-                <Stack
-                  alignItems="stretch"
-                  flexGrow={1}
-                  height={{md: "100%"}}
-                  overflowY={{
-                    md: "scroll",
-                  }}
-                  pb="20"
-                  position="relative"
-                  pt={{base: 4, md: 6}}
-                  px={{base: 4, md: 8}}
-                  spacing="6"
-                  w={{md: "sm"}}
-                >
-                  <Box>
-                    <Text fontSize="xl" fontWeight="bold">
-                      {title}
-                    </Text>
-                    {description && (
-                      <Text color="gray.500" fontSize="sm">
-                        {description}
-                      </Text>
-                    )}
-                  </Box>
-                  <Box>{form}</Box>
-                </Stack>
-                <Button
-                  backgroundColor="primary.500"
-                  bottom={4}
-                  color="white"
-                  display="flex"
-                  flexShrink={0}
-                  h="12"
-                  isLoading={isLoading}
-                  justifyContent="space-between"
-                  left={{base: 4, md: 8}}
-                  marginTop="auto"
-                  position={{
-                    base: "fixed",
-                    md: "absolute",
-                  }}
-                  px="4"
-                  right={{base: 4, md: 8}}
-                  type="submit"
-                  variantColor="primary"
-                  width={{
-                    base: "calc(100% - 2rem)",
-                    md: "calc(100% - 4rem)",
-                  }}
-                  zIndex={1}
-                  onClick={(event) => {
-                    event.stopPropagation();
-
-                    console.log("submit form");
-                    // submit();
-                  }}
-                >
-                  <Flex alignItems="center">
-                    Agregar
+            >
+              <Box overflowY="scroll">
+                {image && (
+                  <Box borderBottomWidth={1} borderColor="gray.100" position="relative">
+                    <GoBackButton
+                      aria-label="Close product details"
+                      icon="arrow-forward"
+                      ml={2}
+                      mt={2}
+                      position="absolute"
+                      variant="ghost"
+                      onClick={onClose}
+                    />
                     <Box
-                      backgroundColor="blackAlpha.300"
-                      borderRadius="md"
-                      fontSize="sm"
-                      fontWeight="normal"
-                      ml="2"
-                      px="2"
-                      py="2px"
-                    >
-                      {formValues.quantity} item{formValues.quantity > 1 ? "s" : ""}
+                      backgroundImage={`url(${image})`}
+                      backgroundPosition="center"
+                      backgroundSize="cover"
+                      height={{
+                        base: 48,
+                        md: "xs",
+                      }}
+                      width={{
+                        base: "100%",
+                        md: "100%",
+                      }}
+                    />
+                  </Box>
+                )}
+                <Box position="relative">
+                  <Stack
+                    alignItems="stretch"
+                    flexGrow={1}
+                    pb="24"
+                    pt={{base: 4, md: 6}}
+                    px={{base: 4, md: 8}}
+                    spacing="6"
+                  >
+                    <Box>
+                      <Text fontSize="xl" fontWeight="bold">
+                        {title}
+                      </Text>
+                      {description && (
+                        <Text color="gray.600" fontSize="sm">
+                          {description}
+                        </Text>
+                      )}
                     </Box>
-                  </Flex>
-                  <Box>${totalPrice}</Box>
-                </Button>
+                    <Box>{form}</Box>
+                  </Stack>
+                </Box>
               </Box>
-            );
-          }}
-        </ProductOptionsForm>
-      </StyledModalContent>
-    </Modal>
+              <Button
+                backgroundColor="primary.500"
+                bottom={4}
+                color="white"
+                display="flex"
+                flexShrink={0}
+                h="12"
+                isLoading={isLoading}
+                justifyContent="space-between"
+                left={{base: 4, md: 8}}
+                marginTop="auto"
+                position="fixed"
+                px="4"
+                right={{base: 4, md: 8}}
+                type="submit"
+                variantColor="primary"
+                width={{
+                  base: "calc(100% - 2rem)",
+                  md: "calc(100% - 4rem)",
+                }}
+                zIndex={1}
+                onClick={(event) => {
+                  event.stopPropagation();
+
+                  console.log("submit form");
+                  // submit();
+                }}
+              >
+                <Flex alignItems="center">
+                  Agregar
+                  <Box
+                    backgroundColor="blackAlpha.300"
+                    borderRadius="md"
+                    fontSize="sm"
+                    fontWeight="normal"
+                    ml="2"
+                    px="2"
+                    py="2px"
+                  >
+                    {formValues.quantity} item{formValues.quantity > 1 ? "s" : ""}
+                  </Box>
+                </Flex>
+                <Box>${totalPrice}</Box>
+              </Button>
+            </DrawerContent>
+          </Drawer>
+        );
+      }}
+    </ProductOptionsForm>
   );
 }
