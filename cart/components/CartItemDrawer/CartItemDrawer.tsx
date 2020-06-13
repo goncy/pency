550a5e3fe289006c46b41da0aaeaa1a1cd3f0cd4
goncy <gonzalo.pozzo4@gmail.com>
2020-06-13 10:38:54 -0300

    Fix layout

--- cart/components/CartItemDrawer/CartItemDrawer.tsx
+++ cart/components/CartItemDrawer/CartItemDrawer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {IDrawer, Text, Stack, Box, Flex} from "@chakra-ui/core";
+import {IDrawer, Text, Stack, Flex} from "@chakra-ui/core";
 
-import Drawer, {DrawerBody} from "~/ui/controls/Drawer";
+import Drawer, {DrawerBody, DrawerFooter} from "~/ui/controls/Drawer";
 import {Product, Variant} from "~/product/types";
 import Image from "~/ui/feedback/Image";
 import ProductVariantForm from "~/product/forms/ProductVariantForm";
@@ -26,51 +26,55 @@ const CartItemDrawer: React.FC<Props> = ({onClose, product, onSubmit, ...props})
     <Drawer id="cart-item" placement="right" size="md" onClose={onClose} {...props}>
       <ProductVariantForm defaultValues={product.options} onSubmit={handleSubmit}>
         {({form, submit, isLoading}) => (
-          <DrawerBody paddingX={0} position="relative">
-            <ArrowLeftIcon
-              cursor="pointer"
-              left={0}
-              padding={4}
-              position="absolute"
-              top={0}
-              onClick={onClose}
-            />
-            {product.image && <Image height="100%" maxHeight="30vh" src={product.image} />}
-            <Flex
-              direction="column"
-              flex={1}
-              marginTop={product.image ? 0 : 8}
-              paddingX={{base: 4, sm: 12}}
-              paddingY={4}
-            >
-              <Stack marginBottom={6} spacing={2}>
-                <Text fontSize="2xl" fontWeight="bold">
-                  {product.title}
-                </Text>
-                <Text color="gray.500">{product.description}</Text>
+          <>
+            <DrawerBody paddingX={0} position="relative">
+              <ArrowLeftIcon
+                cursor="pointer"
+                left={0}
+                padding={4}
+                position="absolute"
+                top={0}
+                onClick={onClose}
+              />
+              {product.image && <Image height="100%" maxHeight="30vh" src={product.image} />}
+              <Stack
+                shouldWrapChildren
+                direction="column"
+                flex={1}
+                marginTop={product.image ? 0 : 8}
+                paddingX={{base: 4, sm: 12}}
+                paddingY={4}
+                spacing={6}
+              >
+                <Stack spacing={0}>
+                  <Text fontSize="2xl" fontWeight="bold">
+                    {product.title}
+                  </Text>
+                  <Text color="gray.500">{product.description}</Text>
+                </Stack>
+                {form}
+                <Flex alignItems="center" justifyContent="space-between">
+                  <FormLabel>Cantidad</FormLabel>
+                  <Stepper min={1} value={count} onChange={setCount} />
+                </Flex>
               </Stack>
-              {form}
-              <Flex alignItems="center" justifyContent="space-between" marginY={6}>
-                <FormLabel>Cantidad</FormLabel>
-                <Stepper min={1} value={count} onChange={setCount} />
-              </Flex>
-              <Box marginTop="auto">
-                <Button
-                  isFullWidth
-                  isLoading={isLoading}
-                  size="lg"
-                  variantColor="primary"
-                  onClick={(event) => {
-                    event.stopPropagation();
+            </DrawerBody>
+            <DrawerFooter>
+              <Button
+                isFullWidth
+                isLoading={isLoading}
+                size="lg"
+                variantColor="primary"
+                onClick={(event) => {
+                  event.stopPropagation();
 
-                    submit();
-                  }}
-                >
-                  Agregar
-                </Button>
-              </Box>
-            </Flex>
-          </DrawerBody>
+                  submit();
+                }}
+              >
+                Agregar
+              </Button>
+            </DrawerFooter>
+          </>
         )}
       </ProductVariantForm>
     </Drawer>
