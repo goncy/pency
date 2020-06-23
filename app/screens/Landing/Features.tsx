22381d6e2c636dfac8be99222c05bcac1a50b59b
Nahuel <nahuecaceres@gmail.com>
2020-06-23 11:51:18 -0300

    Add translations to landing

--- app/screens/Landing/Features.tsx
+++ app/screens/Landing/Features.tsx
@@ -7,32 +7,35 @@ import Content from "./Content";
 import ShoppingIcon from "~/ui/icons/Shopping";
 import ClockIcon from "~/ui/icons/Clock";
 import UsersIcon from "~/ui/icons/Users";
+import {useTranslation} from "~/i18n/hooks";
 
-const Features = () => (
-  <Content>
-    <Grid
-      as="section"
-      gridGap={{base: 8, sm: 6}}
-      paddingBottom={{base: 12, sm: 20}}
-      paddingTop={{base: 20, sm: 32}}
-      templateColumns="repeat(auto-fit, minmax(320px,1fr))"
-    >
-      <Feature icon={ShoppingIcon} title="Mantené tu catálogo actualizado">
-        Cada cambio en tu carta o actualización de precios se refleja al instante.
-      </Feature>
-      <Feature icon={ClockIcon} title="Agilizá la gestión de tu negocio">
-        Recibir pedidos por teléfono es lento, costoso y favorece errores, optimiza tiempo de tus
-        empleados.
-      </Feature>
-      <Feature icon={UsersIcon} title="Alcanzá a muchos más clientes">
-        Agregá, modificá o eliminar productos fácilmente.
-      </Feature>
-      <Feature icon={ShoppingIcon} title="Sin intermediarios">
-        Los clientes son de tu negocio, no pagás comisiones por cada venta y ofreces un mejor
-        precio.
-      </Feature>
-    </Grid>
-  </Content>
-);
+const Features:React.FC = () => {
+  const t = useTranslation();
+
+  return (
+    <Content>
+      <Grid
+        as="section"
+        gridGap={{base: 8, sm: 6}}
+        paddingBottom={{base: 12, sm: 20}}
+        paddingTop={{base: 20, sm: 32}}
+        templateColumns="repeat(auto-fit, minmax(320px,1fr))"
+      >
+        <Feature icon={ShoppingIcon} title={t("app.landing.keepCatalogUpdate.title")}>
+          {t("app.landing.keepCatalogUpdate.description")}
+        </Feature>
+        <Feature icon={ClockIcon} title={t("app.landing.streamlineBusinessManagment.title")}>
+          {t("app.landing.streamlineBusinessManagment.description")}
+        </Feature>
+        <Feature icon={UsersIcon} title={t("app.landing.reachManyCustomers.title")}>
+          {t("app.landing.reachManyCustomers.description")}
+        </Feature>
+        <Feature icon={ShoppingIcon} title={t("app.landing.withoutIntermediaries.title")}>
+          {t("app.landing.withoutIntermediaries.description")}
+        </Feature>
+      </Grid>
+    </Content>
+  )
+};
 
 export default Features;
