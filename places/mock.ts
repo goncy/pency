43a14288f83f2ff85863c54068d4828e9b3717c6
goncy <gonzalo.pozzo4@gmail.com>
2020-06-26 17:29:11 -0300

    Fix build

--- places/mock.ts
+++ places/mock.ts
@@ -1,20 +1,15 @@
-import {Search} from "./types";
-
 export default {
-  search: (query: string): Search => {
-    return {
-      candidates: [
-        {
-          formatted_address: query,
-          geometry: {
-            location: {
-              lat: -33.8599358,
-              lng: 151.2090295,
-            },
+  search: (query: string) => {
+    return [
+      {
+        formatted_address: query,
+        geometry: {
+          location: {
+            lat: -33.8599358,
+            lng: 151.2090295,
           },
         },
-      ],
-      status: "OK",
-    };
+      },
+    ];
   },
 };
