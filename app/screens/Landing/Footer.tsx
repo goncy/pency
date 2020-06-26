919b716b285645fa2ead637a47ff6a7a628d1b8c
Nahuel <nahuecaceres@gmail.com>
2020-06-26 20:43:23 -0300

    Add translations using useTranslation to landing [Footer.tsx]

--- app/screens/Landing/Footer.tsx
+++ app/screens/Landing/Footer.tsx
@@ -9,56 +9,70 @@ import FacebookIcon from "~/ui/icons/Facebook";
 import InstagramIcon from "~/ui/icons/Instagram";
 import WhatsappIcon from "~/ui/icons/WhatsApp";
 import Link from "~/ui/controls/Link";
+import {useTranslation} from "~/i18n/hooks";
 
-const Footer = () => (
-  <Flex alignItems="center" backgroundColor="teal.600" color="white" minHeight={20} paddingY={4}>
-    <Content>
-      <Grid
-        gridGap={4}
-        gridTemplateColumns={{
-          base: "1fr",
-          sm: "1fr auto",
-        }}
-      >
-        <Stack isInline justifyContent="flex-start" spacing={2}>
-          <Link isExternal href="https://twitter.com/Pencyapp">
-            <SocialButton aria-label="Seguinos en Twitter" icon={TwitterIcon} />
-          </Link>
-          <Link isExternal href="https://www.facebook.com/pencyapp">
-            <SocialButton aria-label="Seguinos en Facebook" icon={FacebookIcon} />
-          </Link>
-          <Link isExternal href="https://www.instagram.com/pencyapp">
-            <SocialButton aria-label="Seguinos en Instagram" icon={InstagramIcon} />
-          </Link>
-          <Link isExternal href="https://wa.me/5491173694572">
-            <SocialButton aria-label="Seguinos en Whatsapp" icon={WhatsappIcon} />
-          </Link>
-        </Stack>
+const Footer: React.FC = () => {
+  const t = useTranslation();
+
+  return (
+    <Flex alignItems="center" backgroundColor="teal.600" color="white" minHeight={20} paddingY={4}>
+      <Content>
         <Grid
-          alignItems="center"
-          gridGap={2}
+          gridGap={4}
           gridTemplateColumns={{
             base: "1fr",
-            sm: "repeat(2, 1fr)",
+            sm: "1fr auto",
           }}
-          textAlign={{base: "left", sm: "right"}}
         >
-          <ChakraLink
-            isExternal
-            href="https://drive.google.com/file/d/1F99m25wjRq1QGDrE4Wpf3LmKavN6eg-3/view?usp=sharing"
+          <Stack isInline justifyContent="flex-start" spacing={2}>
+            <Link isExternal href="https://twitter.com/Pencyapp">
+              <SocialButton aria-label={t("app.landing.footerTwitter.title")} icon={TwitterIcon} />
+            </Link>
+            <Link isExternal href="https://www.facebook.com/pencyapp">
+              <SocialButton
+                aria-label={t("app.landing.footerFacebook.title")}
+                icon={FacebookIcon}
+              />
+            </Link>
+            <Link isExternal href="https://www.instagram.com/pencyapp">
+              <SocialButton
+                aria-label={t("app.landing.footerInstragram.title")}
+                icon={InstagramIcon}
+              />
+            </Link>
+            <Link isExternal href="https://wa.me/5491173694572">
+              <SocialButton
+                aria-label={t("app.landing.footerWhatsapp.title")}
+                icon={WhatsappIcon}
+              />
+            </Link>
+          </Stack>
+          <Grid
+            alignItems="center"
+            gridGap={2}
+            gridTemplateColumns={{
+              base: "1fr",
+              sm: "repeat(2, 1fr)",
+            }}
+            textAlign={{base: "left", sm: "right"}}
           >
-            Términos y condiciones
-          </ChakraLink>
-          <ChakraLink
-            isExternal
-            href="https://drive.google.com/file/d/1OWsa_Axqpj3ZtOvby_SbkT2ViCMoXwmx/view?usp=sharing"
-          >
-            Políticas de privacidad
-          </ChakraLink>
+            <ChakraLink
+              isExternal
+              href="https://drive.google.com/file/d/1F99m25wjRq1QGDrE4Wpf3LmKavN6eg-3/view?usp=sharing"
+            >
+              {t("app.landing.termsConditions.title")}
+            </ChakraLink>
+            <ChakraLink
+              isExternal
+              href="https://drive.google.com/file/d/1OWsa_Axqpj3ZtOvby_SbkT2ViCMoXwmx/view?usp=sharing"
+            >
+              {t("app.landing.privacyPolicies.title")}
+            </ChakraLink>
+          </Grid>
         </Grid>
-      </Grid>
-    </Content>
-  </Flex>
-);
+      </Content>
+    </Flex>
+  );
+};
 
 export default Footer;
