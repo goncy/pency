6bc96dee33efd290de5e03e4754355927ef0e1bc
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2020-06-27 14:52:58 -0300

    Implemented generic error control

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,16 @@
 import React from "react";
 import App from "next/app";
-import * as Sentry from "@sentry/browser";
 import {Flex, ThemeProvider, CSSReset} from "@chakra-ui/core";
+
+import ErrorService, {ErrorInfo, SentryErrorProvider} from "../errors";
 
 import ErrorScreen from "./_error";
 
-if (process.env.NODE_ENV === "production" && process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-  });
-}
+const errorService = new ErrorService(new SentryErrorProvider());
 
 export default class Pency extends App {
-  componentDidCatch(error, errorInfo) {
-    if (process.env.NODE_ENV === "production" && process.env.SENTRY_DSN) {
-      Sentry.withScope((scope) => {
-        scope.setTag("origin", "componentDidCatch");
-
-        Object.keys(errorInfo).forEach((key) => {
-          scope.setExtra(key, errorInfo[key]);
-        });
-
-        scope.setExtra("error", error);
-
-        Sentry.captureException(error);
-      });
-    }
-
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    errorService.reportError(error, errorInfo);
     super.componentDidCatch(error, errorInfo);
   }
 
