21bdc860e5e66ed2abcc15716f22eb420f3b876d
Nahuel <nahuecaceres@gmail.com>
2020-06-26 21:26:25 -0300

    Add translations using useTranslation to landing [NavBar.tsx]

--- app/screens/Landing/Navbar.tsx
+++ app/screens/Landing/Navbar.tsx
@@ -6,41 +6,46 @@ import Content from "./Content";
 import Link from "~/ui/controls/Link";
 import Logo from "~/ui/static/Logo";
 import WhatsappIcon from "~/ui/icons/WhatsApp";
+import {useTranslation} from "~/i18n/hooks";
 
-const Navbar = () => (
-  <Content
-    alignItems="center"
-    as="nav"
-    backgroundColor="white"
-    display="flex"
-    justifyContent="space-between"
-    paddingY={{base: 4, sm: 5}}
-  >
-    <Logo />
-    <Grid alignItems="center" gridAutoFlow="column" gridGap={{base: 4, sm: 8}}>
-      <Link
-        isExternal
-        _hover={{color: "teal.700"}}
-        color="gray.900"
-        fontWeight={500}
-        href={`https://wa.me/${process.env.MANTAINER_PHONE}`}
-        transition="color 0.15s ease-out"
-      >
-        <Stack isInline>
-          <WhatsappIcon width={6} />
-          <Text>Soporte</Text>
-        </Stack>
-      </Link>
-      <Link
-        isExternal
-        href="https://docs.google.com/forms/d/1P1o0HVHDluk-VMHHRLFVPn_TDKkqg_JhlMBmclcd6Co"
-      >
-        <Button d={{base: "none", sm: "block"}} variantColor="teal">
-          Crear tienda
-        </Button>
-      </Link>
-    </Grid>
-  </Content>
-);
+const Navbar: React.FC = () => {
+  const t = useTranslation();
+
+  return (
+    <Content
+      alignItems="center"
+      as="nav"
+      backgroundColor="white"
+      display="flex"
+      justifyContent="space-between"
+      paddingY={{base: 4, sm: 5}}
+    >
+      <Logo />
+      <Grid alignItems="center" gridAutoFlow="column" gridGap={{base: 4, sm: 8}}>
+        <Link
+          isExternal
+          _hover={{color: "teal.700"}}
+          color="gray.900"
+          fontWeight={500}
+          href={`https://wa.me/${process.env.MANTAINER_PHONE}`}
+          transition="color 0.15s ease-out"
+        >
+          <Stack isInline>
+            <WhatsappIcon width={6} />
+            <Text>{t("app.landing.supportByWhatsapp.description")}</Text>
+          </Stack>
+        </Link>
+        <Link
+          isExternal
+          href="https://docs.google.com/forms/d/1P1o0HVHDluk-VMHHRLFVPn_TDKkqg_JhlMBmclcd6Co"
+        >
+          <Button d={{base: "none", sm: "block"}} variantColor="teal">
+            {t("app.landing.createStore.description")}
+          </Button>
+        </Link>
+      </Grid>
+    </Content>
+  );
+};
 
 export default Navbar;
