2080c029dbcd171e0a6b4994c6b45b7e176224cf
Nahuel <nahuecaceres@gmail.com>
2020-06-26 21:12:37 -0300

    Add translations using useTranslation to landing [Header.tsx]

--- app/screens/Landing/Header.tsx
+++ app/screens/Landing/Header.tsx
@@ -4,81 +4,86 @@ import {Flex, Box, Button, Text, Image} from "@chakra-ui/core";
 import Content from "./Content";
 
 import Link from "~/ui/controls/Link";
+import {useTranslation} from "~/i18n/hooks";
 
-const Header: React.FC = () => (
-  <Flex alignItems="center" as="header" backgroundColor="teal.600" color="white" paddingY={8}>
-    <Content alignItems="center" display="flex" flexDirection={{base: "column", md: "row"}}>
-      <Box maxWidth={{base: "auto", sm: 500, lg: 580, xl: 640}}>
-        <Text
-          as="h1"
-          fontSize={{base: "2xl", md: "4xl", lg: "5xl", xl: "6xl"}}
-          fontWeight={500}
-          lineHeight={"130%"}
-          marginBottom={2}
-        >
-          Ahorrá tiempo y dinero con tu tienda Pency
-        </Text>
-        <Text
-          as="h2"
-          color="teal.50"
-          fontSize={{base: "lg", sm: "lg", md: "xl", lg: "2xl", xl: "3xl"}}
-        >
-          Publicá tu catálogo online y recibí por Whatsapp los pedidos de tus clientes ¡GRATIS!
-        </Text>
-        <Box display={{base: "none", sm: "block"}} marginTop={10}>
-          <Link
-            isExternal
-            href="https://docs.google.com/forms/d/1P1o0HVHDluk-VMHHRLFVPn_TDKkqg_JhlMBmclcd6Co"
+const Header: React.FC = () => {
+  const t = useTranslation();
+
+  return (
+    <Flex alignItems="center" as="header" backgroundColor="teal.600" color="white" paddingY={8}>
+      <Content alignItems="center" display="flex" flexDirection={{base: "column", md: "row"}}>
+        <Box maxWidth={{base: "auto", sm: 500, lg: 580, xl: 640}}>
+          <Text
+            as="h1"
+            fontSize={{base: "2xl", md: "4xl", lg: "5xl", xl: "6xl"}}
+            fontWeight={500}
+            lineHeight={"130%"}
+            marginBottom={2}
           >
-            <Button
-              _hover={{
-                boxShadow: "xl",
-                transform: "translateY(-1px)",
-              }}
-              backgroundColor="white"
-              color="black"
-              size="lg"
+            {t("app.landing.saveTimeMoney.description")}
+          </Text>
+          <Text
+            as="h2"
+            color="teal.50"
+            fontSize={{base: "lg", sm: "lg", md: "xl", lg: "2xl", xl: "3xl"}}
+          >
+            {t("app.landing.publishCatalogOnline.description")}
+          </Text>
+          <Box display={{base: "none", sm: "block"}} marginTop={10}>
+            <Link
+              isExternal
+              href="https://docs.google.com/forms/d/1P1o0HVHDluk-VMHHRLFVPn_TDKkqg_JhlMBmclcd6Co"
             >
-              Crear tienda
-            </Button>
-          </Link>
-          <Link isExternal href="https://pency.app/demo">
-            <Button
-              _hover={{
-                backgroundColor: "rgba(255,255,255,0.15)",
-                boxShadow: "xl",
-                transform: "translateY(-1px)",
-              }}
-              color="white"
-              marginLeft={{base: 0, sm: 6}}
-              size="lg"
-              variant="outline"
-            >
-              Ver tienda de prueba
-            </Button>
-          </Link>
+              <Button
+                _hover={{
+                  boxShadow: "xl",
+                  transform: "translateY(-1px)",
+                }}
+                backgroundColor="white"
+                color="black"
+                size="lg"
+              >
+                {t("app.landing.createStore.description")}
+              </Button>
+            </Link>
+            <Link isExternal href="https://pency.app/demo">
+              <Button
+                _hover={{
+                  backgroundColor: "rgba(255,255,255,0.15)",
+                  boxShadow: "xl",
+                  transform: "translateY(-1px)",
+                }}
+                color="white"
+                marginLeft={{base: 0, sm: 6}}
+                size="lg"
+                variant="outline"
+              >
+                {t("app.landing.seeTestShop.description")}
+              </Button>
+            </Link>
+          </Box>
         </Box>
-      </Box>
-      <Flex
-        alignItems="center"
-        backgroundImage="url('/assets/landing/bg.png')"
-        backgroundPosition="right center"
-        backgroundRepeat="no-repeat"
-        backgroundSize="contain"
-        flex={1}
-        justifyContent={{base: "center", sm: "center", md: "flex-end"}}
-        marginRight={{base: -4, sm: 0}}
-        marginTop={{base: 10, sm: 0}}
-      >
-        <Image
-          marginBottom={{base: -20, sm: -24}}
-          maxWidth={{base: "80vw", sm: "xs", lg: "md"}}
-          paddingTop={{base: 2, sm: 0}}
-          src="/assets/landing/pency.png"
-        />
-      </Flex>
-    </Content>
-  </Flex>
-);
+        <Flex
+          alignItems="center"
+          backgroundImage="url('/assets/landing/bg.png')"
+          backgroundPosition="right center"
+          backgroundRepeat="no-repeat"
+          backgroundSize="contain"
+          flex={1}
+          justifyContent={{base: "center", sm: "center", md: "flex-end"}}
+          marginRight={{base: -4, sm: 0}}
+          marginTop={{base: 10, sm: 0}}
+        >
+          <Image
+            marginBottom={{base: -20, sm: -24}}
+            maxWidth={{base: "80vw", sm: "xs", lg: "md"}}
+            paddingTop={{base: 2, sm: 0}}
+            src="/assets/landing/pency.png"
+          />
+        </Flex>
+      </Content>
+    </Flex>
+  );
+};
 
 export default Header;
