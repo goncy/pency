355820ad272296729ec81e4d4a7a8f631f989a00
goncy <gonzalo.pozzo4@gmail.com>
2020-06-29 14:28:21 -0300

    Fix api tenant method

--- tenant/api/routes/index.ts
+++ tenant/api/routes/index.ts
@@ -7,7 +7,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     return api
       .list()
-      .then((tenants) => res.status(200).json(tenants.map(schemas.client.fetch.cast)))
+      .then((tenants) =>
+        res.status(200).json(tenants.map((tenant) => schemas.client.fetch.cast(tenant))),
+      )
       .catch(({status, statusText}) => res.status(status).end(statusText));
   }
 
