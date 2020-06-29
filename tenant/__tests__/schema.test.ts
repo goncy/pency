2b3caaabf04229a987b013c91d07e1aa5705d483
goncy <gonzalo.pozzo4@gmail.com>
2020-06-29 15:52:18 -0300

    Fix tenant api

--- tenant/__tests__/schema.test.ts
+++ tenant/__tests__/schema.test.ts
@@ -173,17 +173,48 @@ describe("schemas", () => {
 
         expect(schemas.server.create.cast(actual)).toEqual(expected);
       });
+
+      it("should normalize location if present", () => {
+        const actual = {
+          slug: "goncy",
+          location: {
+            address: "some-address",
+            coordinates: {
+              lat: "100",
+              lng: "100",
+            },
+          },
+        };
+        const expected = {
+          slug: "goncy",
+          color: "teal",
+          country: "AR",
+          description: "Armá tu tienda y recibí los pedidos via WhatsApp",
+          keywords: "pency, tienda, online, whatsapp, delivery, pedidos, shop",
+          mercadopago: null,
+          phone: "5491173694572",
+          title: "Pency - Tu tienda online fácil",
+          location: {
+            address: "some-address",
+            coordinates: {
+              lat: 100,
+              lng: 100,
+            },
+          },
+          flags: [],
+        };
+
+        expect(schemas.server.create.cast(actual)).toEqual(expected);
+      });
     });
 
     describe("mercadopago", () => {
       it("should normalize any to server tenant mercadopago", () => {
-        const actual = {};
+        const actual = {
+          mercadopago: null,
+        };
         const expected = {
-          mercadopago: {
-            expiration: 0,
-            refresh: "",
-            token: "",
-          },
+          mercadopago: null,
         };
 
         expect(schemas.server.mercadopago.cast(actual)).toEqual(expected);
@@ -221,7 +252,7 @@ describe("schemas", () => {
           ...actual,
           banner: "",
           category: "",
-          country: "",
+          country: "AR",
           description: "",
           facebook: "",
           fields: [],
@@ -253,7 +284,7 @@ describe("schemas", () => {
           phone: "1144444444",
           banner: "",
           category: "",
-          country: "",
+          country: "AR",
           description: "",
           facebook: "",
           fields: [],
@@ -291,7 +322,7 @@ describe("schemas", () => {
           phone: "1144444444",
           banner: "",
           category: "",
-          country: "",
+          country: "AR",
           description: "",
           facebook: "",
           fields: [],
@@ -336,7 +367,7 @@ describe("schemas", () => {
           phone: "1144444444",
           banner: "",
           category: "",
-          country: "",
+          country: "AR",
           description: "",
           facebook: "",
           fields: [],
@@ -351,6 +382,71 @@ describe("schemas", () => {
               lng: 10.001,
             },
           },
+          logo: "",
+          mercadopago: false,
+          title: "",
+          twitter: "",
+        };
+
+        expect(schemas.client.fetch.cast(actual)).toEqual(expected);
+      });
+
+      it("should normalize any to client tenant allowing null location", () => {
+        const actual = {
+          id: "some-id",
+          slug: "some-slug",
+          color: "teal",
+          phone: 1144444444,
+          highlight: "Some highlight",
+          flags: [1, 2, 3],
+          location: null,
+        };
+        const expected = {
+          ...actual,
+          phone: "1144444444",
+          banner: "",
+          category: "",
+          country: "AR",
+          description: "",
+          facebook: "",
+          fields: [],
+          flags: ["1", "2", "3"],
+          hook: "",
+          instagram: "",
+          keywords: "",
+          location: null,
+          logo: "",
+          mercadopago: false,
+          title: "",
+          twitter: "",
+        };
+
+        expect(schemas.client.fetch.cast(actual)).toEqual(expected);
+      });
+
+      it("should normalize any to client tenant adding location when not present", () => {
+        const actual = {
+          id: "some-id",
+          slug: "some-slug",
+          color: "teal",
+          phone: 1144444444,
+          highlight: "Some highlight",
+          flags: [1, 2, 3],
+        };
+        const expected = {
+          ...actual,
+          phone: "1144444444",
+          banner: "",
+          category: "",
+          country: "AR",
+          description: "",
+          facebook: "",
+          fields: [],
+          flags: ["1", "2", "3"],
+          hook: "",
+          instagram: "",
+          keywords: "",
+          location: null,
           logo: "",
           mercadopago: false,
           title: "",
