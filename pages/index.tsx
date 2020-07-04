fd53c4ef83fc3bd7cec22eb707e9a2d483a9abf2
goncy <gonzalo.pozzo4@gmail.com>
2020-07-04 09:24:10 -0300

    Add sitemap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,40 @@
+import fs from "fs";
+
 import React from "react";
+import {GetStaticProps} from "next";
 
 import LandingScreen from "~/app/screens/Landing";
 import LandingLayout from "~/app/layouts/LandingLayout";
 import {Provider as I18nProvider} from "~/i18n/context";
+import {ServerTenant} from "~/tenant/types";
+import api from "~/tenant/api/server";
+import {buildSitemap} from "~/utils/sitemap";
 
-const LandingRoute: React.FC = () => (
+interface Props {
+  stores: ServerTenant[];
+}
+
+const LandingRoute: React.FC<Props> = () => (
   <LandingLayout>
     <I18nProvider detect>
       <LandingScreen />
     </I18nProvider>
   </LandingLayout>
 );
+
+export const getStaticProps: GetStaticProps = async () => {
+  // Get stores from api
+  const stores: ServerTenant[] = await api.list();
+
+  // Build sitemap
+  fs.writeFileSync("public/sitemap.xml", buildSitemap(stores));
+
+  // Return stores so we can build a directory
+  return {
+    props: {
+      stores,
+    },
+  };
+};
 
 export default LandingRoute;
