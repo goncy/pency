c840386a8077b3a1aa8be6f860e8075fb4f25050
goncy <gonzalo.pozzo4@gmail.com>
2020-07-04 15:14:36 -0300

    Update alignment

--- app/screens/Landing/Directory.tsx
+++ app/screens/Landing/Directory.tsx
@@ -136,9 +136,7 @@ const Directory: React.FC<Props> = ({tenants}) => {
           borderColor="gray.100"
           borderWidth={1}
           boxShadow="xl"
-          maxHeight={480}
           maxWidth={{base: 300, sm: 480}}
-          overflowY="auto"
           spacing={0}
           width="100%"
         >
@@ -151,37 +149,39 @@ const Directory: React.FC<Props> = ({tenants}) => {
             onChange={handleQueryChange}
             onClear={handleQueryClear}
           />
-          {filtered.length ? (
-            filtered.map(({title, logo, slug, category}) => (
-              <Link key={slug} isExternal href={`/${slug}`}>
-                <PseudoBox
-                  _hover={{backgroundColor: "gray.50"}}
-                  _notLast={{borderBottomWidth: 1}}
-                  padding={4}
-                  transition="background-color 0.25s"
-                >
-                  <Stack isInline alignItems="center" spacing={2}>
-                    <Image borderWidth={1} height={16} rounded="full" src={logo} width={16} />
-                    <Stack spacing={0}>
-                      <Text fontSize="lg" fontWeight={500} lineHeight="normal">
-                        {title}
-                      </Text>
-                      <Text color="gray.500" textAlign="left">
-                        /{slug}
-                      </Text>
-                      <Text color="teal.500" fontSize="sm" textAlign="left">
-                        {t(`catalogs.categories.${category}`)}
-                      </Text>
+          <Stack maxHeight={480} overflowY="auto" spacing={0} textAlign="left">
+            {filtered.length ? (
+              filtered.map(({title, logo, slug, category}) => (
+                <Link key={slug} isExternal href={`/${slug}`}>
+                  <PseudoBox
+                    _hover={{backgroundColor: "gray.50"}}
+                    _notLast={{borderBottomWidth: 1}}
+                    padding={4}
+                    transition="background-color 0.25s"
+                  >
+                    <Stack isInline alignItems="center" spacing={2}>
+                      <Image borderWidth={1} height={16} rounded="full" src={logo} width={16} />
+                      <Stack spacing={0}>
+                        <Text fontSize="lg" fontWeight={500} lineHeight="normal">
+                          {title}
+                        </Text>
+                        <Text color="gray.500" textAlign="left">
+                          /{slug}
+                        </Text>
+                        <Text color="teal.500" fontSize="sm" textAlign="left">
+                          {t(`catalogs.categories.${category}`)}
+                        </Text>
+                      </Stack>
                     </Stack>
-                  </Stack>
-                </PseudoBox>
-              </Link>
-            ))
-          ) : (
-            <Stack alignItems="center" color="gray.400" justifyContent="center" padding={4}>
-              <Text>{t("landing.directory.noResults")}</Text>
-            </Stack>
-          )}
+                  </PseudoBox>
+                </Link>
+              ))
+            ) : (
+              <Stack alignItems="center" color="gray.400" justifyContent="center" padding={4}>
+                <Text>{t("landing.directory.noResults")}</Text>
+              </Stack>
+            )}
+          </Stack>
         </Stack>
         {coordinates ? (
           <Text color="gray.400" fontSize="sm">
