fd9b236e10f08799eb443da7374e60488a0dc934
goncy <gonzalo.pozzo4@gmail.com>
2020-07-07 00:46:06 -0300

    Add base url to sitemap

--- utils/sitemap.ts
+++ utils/sitemap.ts
@@ -3,6 +3,9 @@ import {ClientTenant} from "~/tenant/types";
 export function buildSitemap(stores: ClientTenant[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+          <loc>${process.env.APP_URL}</loc>
+      </url>
       ${stores
         .map(({slug}) => {
           return `
