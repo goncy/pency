ec59753add396cefcc954473700e6977848ca9f0
goncy <gonzalo.pozzo4@gmail.com>
2020-07-14 14:52:35 -0300

    Update revalidation times

--- tenant/constants.ts
+++ tenant/constants.ts
@@ -1,7 +1,7 @@
 import {ClientTenant} from "./types";
 
 export const REVALIDATION_TIMES: Record<ClientTenant["tier"], number> = {
-  free: 43200,
+  free: 10,
   enterpreneur: 3600,
   commercial: 10,
 };
