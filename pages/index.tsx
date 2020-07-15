82371d91575563282dbcc3191a1225ef32832224
goncy <gonzalo.pozzo4@gmail.com>
2020-07-15 12:23:23 -0300

    Remove fetch from login

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import fs from "fs";
 import React from "react";
 import {GetStaticProps} from "next";
 
-import fetch from "~/utils/fetch";
 import LandingScreen from "~/app/screens/Landing";
 import LandingLayout from "~/app/layouts/LandingLayout";
 import {Provider as I18nProvider} from "~/i18n/context";
 import {ClientTenant} from "~/tenant/types";
 import {buildSitemap} from "~/utils/sitemap";
 import {filterByRelevant} from "~/tenant/selectors";
+import tenantApi from "~/tenant/api/server";
+import schemas from "~/tenant/schemas";
 
 interface Props {
   tenants: ClientTenant[];
@@ -33,30 +34,24 @@ export const getStaticProps: GetStaticProps = async () => {
     };
   }
 
-  try {
-    // Get stores from api
-    const tenants: ClientTenant[] = await fetch("GET", `${process.env.APP_URL}/api/tenant`);
+  // Get stores from db
+  const tenants: ClientTenant[] = await tenantApi
+    .list()
+    // Cast them as client tenants
+    .then((tenants) => tenants.map((tenant) => schemas.client.fetch.cast(tenant)));
 
-    // Get just important ones
-    const filtered = filterByRelevant(tenants);
+  // Get just important ones
+  const filtered = filterByRelevant(tenants);
 
-    // Build sitemap
-    fs.writeFileSync("public/sitemap.xml", buildSitemap(filtered));
+  // Build sitemap
+  fs.writeFileSync("public/sitemap.xml", buildSitemap(filtered));
 
-    // Return stores so we can build a directory
-    return {
-      props: {
-        tenants: filtered,
-      },
-    };
-  } catch (e) {
-    // Allow this to fail
-    return {
-      props: {
-        tenants: [],
-      },
-    };
-  }
+  // Return stores so we can build a directory
+  return {
+    props: {
+      tenants: filtered,
+    },
+  };
 };
 
 export default LandingRoute;
