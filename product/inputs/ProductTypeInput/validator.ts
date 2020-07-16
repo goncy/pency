c556ba0dd98d3f833732ab902a3c9a7472968cae
goncy <gonzalo.pozzo4@gmail.com>
2020-07-16 11:06:12 -0300

    Fix featured

--- product/inputs/ProductTypeInput/validator.ts
+++ product/inputs/ProductTypeInput/validator.ts
@@ -2,7 +2,7 @@ import {Product} from "~/product/types";
 
 export default function validator(variants: Product["options"] = []) {
   return function (type: Product["type"]) {
-    if (!variants.length || !variants.some((variant) => variant.required)) {
+    if (type === "variant" && (!variants.length || !variants.some((variant) => variant.required))) {
       return "Al menos una variante requerida es necesaria para este tipo de producto";
     }
 
