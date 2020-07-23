41c706962e03b1448108f8fe7162cf1d7c8079fe
goncy <gonzalo.pozzo4@gmail.com>
2020-07-23 16:49:07 -0300

    Fix build

--- tenant/mock.ts
+++ tenant/mock.ts
@@ -16,6 +16,7 @@ const mock = {
         instagram: faker.internet.userName(),
         facebook: faker.internet.userName(),
         createdAt: faker.random.number(999999999),
+        updatedAt: faker.random.number(999999999),
         tierUntil: faker.random.number(999999999),
         logo: "//placehold.it/128x128",
         banner: "//placehold.it/256x128",
